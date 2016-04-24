908d9edc9f078a83663fbd0ad8ee4857d44f9295
Theo Pack Test <tf.pack@gmail.com>
2016-04-24 09:51:53 +0200

    Use count

--- coreos_digitalocean.tf
+++ coreos_digitalocean.tf
@@ -2,12 +2,21 @@ variable "do_token" {}
 variable "domain_name" {}
 variable "ssh_fingerprint" {}
 
+variable "service_count" {
+  default = 3
+}
+
+variable "worker_count" {
+  default = 3
+}
+
 provider "digitalocean" {
   token = "${var.do_token}"
 }
 
-resource "digitalocean_droplet" "coreos1" {
-  name = "coreos1"
+resource "digitalocean_droplet" "coreos" {
+  count = "${var.service_count}"
+  name = "${format("coreos-%02d", count.index)}"
   size = "512mb"
   image = "coreos-stable"
   region = "fra1"
@@ -16,28 +25,9 @@ resource "digitalocean_droplet" "coreos1" {
   user_data = "${file(".build/service.yml")}"
 }
 
-resource "digitalocean_droplet" "coreos2" {
-  name = "coreos2"
-  size = "512mb"
-  image = "coreos-stable"
-  region = "fra1"
-  private_networking = true
-  ssh_keys = ["${var.ssh_fingerprint}"]
-  user_data = "${file(".build/service.yml")}"
-}
-
-resource "digitalocean_droplet" "coreos3" {
-  name = "coreos3"
-  size = "512mb"
-  image = "coreos-stable"
-  region = "fra1"
-  private_networking = true
-  ssh_keys = ["${var.ssh_fingerprint}"]
-  user_data = "${file(".build/service.yml")}"
-}
-
-resource "digitalocean_droplet" "coreos-worker1" {
-  name = "coreos-worker1"
+resource "digitalocean_droplet" "coreos-worker" {
+  count = "${var.worker_count}"
+  name = "${format("coreos-worker-%02d", count.index)}"
   size = "512mb"
   image = "coreos-stable"
   region = "fra1"
@@ -46,88 +36,18 @@ resource "digitalocean_droplet" "coreos-worker1" {
   user_data = "${file(".build/worker.yml")}"
 }
 
-resource "digitalocean_droplet" "coreos-worker2" {
-  name = "coreos-worker2"
-  size = "512mb"
-  image = "coreos-stable"
-  region = "fra1"
-  private_networking = true
-  ssh_keys = ["${var.ssh_fingerprint}"]
-  user_data = "${file(".build/worker.yml")}"
-}
-
-resource "digitalocean_droplet" "coreos-worker3" {
-  name = "coreos-worker3"
-  size = "512mb"
-  image = "coreos-stable"
-  region = "fra1"
-  private_networking = true
-  ssh_keys = ["${var.ssh_fingerprint}"]
-  user_data = "${file(".build/worker.yml")}"
-}
-
-resource "digitalocean_record" "coreos1" {
+resource "digitalocean_record" "coreos" {
+    count = "${var.service_count}"
     domain = "${var.domain_name}"
     type = "A"
-    name = "coreos1"
-    value = "${digitalocean_droplet.coreos1.ipv4_address}"
+    name = "${format("coreos-%02d", count.index)}"
+    value = "${element(digitalocean_droplet.coreos.*.ipv4_address, count.index)}"
 }
 
-resource "digitalocean_record" "coreos2" {
+resource "digitalocean_record" "coreos-worker" {
+    count = "${var.worker_count}"
     domain = "${var.domain_name}"
     type = "A"
-    name = "coreos2"
-    value = "${digitalocean_droplet.coreos2.ipv4_address}"
-}
-
-resource "digitalocean_record" "coreos3" {
-    domain = "${var.domain_name}"
-    type = "A"
-    name = "coreos3"
-    value = "${digitalocean_droplet.coreos3.ipv4_address}"
-}
-
-resource "digitalocean_record" "coreos-worker1" {
-    domain = "${var.domain_name}"
-    type = "A"
-    name = "coreos-worker1"
-    value = "${digitalocean_droplet.coreos-worker1.ipv4_address}"
-}
-
-resource "digitalocean_record" "coreos-worker2" {
-    domain = "${var.domain_name}"
-    type = "A"
-    name = "coreos-worker2"
-    value = "${digitalocean_droplet.coreos-worker2.ipv4_address}"
-}
-
-resource "digitalocean_record" "coreos-worker3" {
-    domain = "${var.domain_name}"
-    type = "A"
-    name = "coreos-worker3"
-    value = "${digitalocean_droplet.coreos-worker3.ipv4_address}"
-}
-
-output "address_coreos1" {
-  value = "${digitalocean_droplet.coreos1.ipv4_address}"
-}
-
-output "address_coreos2" {
-  value = "${digitalocean_droplet.coreos2.ipv4_address}"
-}
-
-output "address_coreos3" {
-  value = "${digitalocean_droplet.coreos3.ipv4_address}"
-}
-
-output "address_coreos-worker1" {
-  value = "${digitalocean_droplet.coreos-worker1.ipv4_address}"
-}
-
-output "address_coreos-worker2" {
-  value = "${digitalocean_droplet.coreos-worker2.ipv4_address}"
-}
-
-output "address_coreos-worker3" {
-  value = "${digitalocean_droplet.coreos-worker3.ipv4_address}"
+    name =  "${format("coreos-worker-%02d", count.index)}"
+    value = "${element(digitalocean_droplet.coreos-worker.*.ipv4_address, count.index)}"
 }
