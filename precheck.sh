b357fe868ac1ae30a2000a89d5ed0f58c55e298d
Theo Pack Test <tf.pack@gmail.com>
2016-04-26 18:48:41 +0200

    Only regenerate configs if they do not exists

--- precheck.sh
+++ precheck.sh
@@ -7,9 +7,16 @@ if [ ! -d ".build" ]; then
   mkdir .build
 fi
 
-ETCD_DISCOVERY=$(curl -w "\n" 'https://discovery.etcd.io/new?size=3')
+if [ ! -f ".build/service.yml"  ]; then
+  echo "Remove old files"
 
-echo "use $ETCD_DISCOVERY"
+  rm .build/worker.yml
+  rm .build/service.yml
+  
+  ETCD_DISCOVERY=$(curl -w "\n" 'https://discovery.etcd.io/new?size=3')
 
-sed "s%<your_discovery_url>%$ETCD_DISCOVERY%g" <worker.yml >.build/worker.yml
-sed "s%<your_discovery_url>%$ETCD_DISCOVERY%g" <service.yml >.build/service.yml
+  echo "use $ETCD_DISCOVERY"
+
+  sed "s%<your_discovery_url>%$ETCD_DISCOVERY%g" <worker.yml >.build/worker.yml
+  sed "s%<your_discovery_url>%$ETCD_DISCOVERY%g" <service.yml >.build/service.yml
+fi
