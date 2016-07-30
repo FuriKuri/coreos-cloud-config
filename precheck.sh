fab329b30caa7255c60fb77aae2b3e6fa3e0b7fb
Theo Pack Test <tf.pack@gmail.com>
2016-07-30 09:48:46 +0200

    Fix script

--- precheck.sh
+++ precheck.sh
@@ -7,15 +7,15 @@ if [ ! -d ".build" ]; then
   mkdir .build
 fi
 
-if [ ! -f ".build/service.yml" || ! -f ".build/worker.yml"  ]; then
+if [ ! -f ".build/service.yml" -o ! -f ".build/worker.yml" ]; then
   echo "Remove old files"
 
-  if [ ! -f ".build/worker.yml"  ]; then
+  if [ ! -f ".build/worker.yml" ]; then
     echo "Remove old worker.yml"
     rm .build/worker.yml
   fi
 
-  if [ ! -f ".build/service.yml"  ]; then
+  if [ ! -f ".build/service.yml" ]; then
     echo "Remove old service.yml"
     rm .build/service.yml
   fi
